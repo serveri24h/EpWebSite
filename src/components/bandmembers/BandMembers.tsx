b5e769a6595e4f11d839edaba895f252382724b4
Severi <severi.vapalahti@reagle.fi>
2025-07-15 22:36:40 +0300

    chore: fixe linting issues

--- src/components/bandmembers/BandMembers.tsx
+++ src/components/bandmembers/BandMembers.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import './BandMembers.css';
 import BandMemberDetails, {MemberConfig, MemberConfigList} from './BandMemberDetails';
 
