4bd307ae77304d950bf81fe250f6fd029b99d089
Severi Pytäkone <severi.vapalahti@gmail.com>
2025-07-24 18:49:39 +0300

    chore: added credits

--- src/components/pagewrapper/PageWrapper.tsx
+++ src/components/pagewrapper/PageWrapper.tsx
@@ -1,6 +1,13 @@
 import React, { ReactNode } from 'react';
 import './PageWrapper.css';
 
+const Credits:React.FC = () => {
+  return <div className='credits-container'>
+    <h4> Kansitaide - Irene Vapalahti </h4>
+    <h4> Kuvat - Otso Karhu </h4>
+  </div>
+} 
+
 export const FullPageWrapper:React.FC<{children: ReactNode[]}> = ({children}) => {
     if (!children || children.length!==2) {
         throw Error("Fuck")
@@ -11,6 +18,7 @@ export const FullPageWrapper:React.FC<{children: ReactNode[]}> = ({children}) =>
         <div className='page-wrapper-child'>
           {children[1] /* Remaining children */}
         </div>
+        <Credits/>
       </div>
     );
   };
